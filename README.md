05eeae3e68063da9015701e25015fa36de7743d3
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-03-26 13:32:45 -0300

    (Version 1.0) The beginning

---
+++ README.md
@@ -0,0 +1,27 @@
+# Veja a versão em Português <a href="README-ptbr.md">aqui</a>
+
+## GitStats
+
+### What is this? 🤔 
+This project is made to get your Github profile/repos stats in a simple way on Discord. 
+
+### Where can I acess it? 🖥
+You need to download the files of this repository.
+
+### Which tecnologies were used to build it? 🚀 
+It currently uses Javascript with NodeJS.
+
+### How to run 🏃
+
+     Download the files on this repo.
+     Get a discord bot key on <a href="https://discord.com/developers/applications">here</a>.
+     Go to the repo directory on your PC and run "npm install" and "npx nodemon gitstats.mjs".
+
+### Here are some images of the project running: ⚡️
+
+![image](https://github.com/RuanEmanuell/challenge_coorlab/assets/113607857/149a1423-ca20-4233-bcf0-6de95d6bfde0)
+![image](https://github.com/RuanEmanuell/challenge_coorlab/assets/113607857/ac66d150-83d9-46d3-903f-3272545fb28a)
+
+
+
+
---
+++ gitstats.mjs
@@ -0,0 +1,45 @@
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const key = process.env.PRIVATE_KEY;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once("ready", () => {
+    console.log("Ready to rumble!");
+});
+
+client.login(key);
+
+client.on('messageCreate', async message => {
+    const messageText = message.content;
+    if (!message.author.bot) {
+        if (messageText.substring(0, messageText.indexOf(" ")) === "!profile") {
+            const desiredProfile = messageText.substring(messageText.indexOf(" ") + 1, messageText.length);
+            try {
+                const url = `https://api.github.com/users/${desiredProfile}`
+                const response = await fetch(url);
+                const data = await response.json();
+                console.log(data);
+                const embed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle(`${data.login} Profile`)
+                    .setURL(`https://github.com/${desiredProfile}`)
+                    .setThumbnail("https://i.pinimg.com/originals/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.png")
+                    .addFields(
+                        { name: "Nickname", value: `${data.login}` },
+                        { name: "Name", value: `${data.name}` },
+                        { name: "Repositories (Public)", value: `${data.public_repos}` },
+                        { name: "Followers", value: `${data.followers}` },
+                        { name: "Following", value: `${data.following}` },
+                        { name: "Website", value: `${data.blog ? data.blog : "None"}` }
+                    )
+                    .setImage(`${data.avatar_url}`);
+                await message.reply({ embeds: [embed] });
+            } catch (error) {
+                message.reply(`An error ocurred. ${error}`);
+            }
+        }
+    }
+});
