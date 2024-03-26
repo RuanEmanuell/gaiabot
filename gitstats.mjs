c00fa88aa9a66684dfae83f1f86c01c248f6b090
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-03-26 13:54:45 -0300

    (Version 1.0) Adding support for multiple commands

--- gitstats.mjs
+++ gitstats.mjs
@@ -14,32 +14,42 @@ client.login(key);
 
 client.on('messageCreate', async message => {
     const messageText = message.content;
-    if (!message.author.bot) {
-        if (messageText.substring(0, messageText.indexOf(" ")) === "!profile") {
-            const desiredProfile = messageText.substring(messageText.indexOf(" ") + 1, messageText.length);
-            try {
-                const url = `https://api.github.com/users/${desiredProfile}`
-                const response = await fetch(url);
-                const data = await response.json();
-                console.log(data);
-                const embed = new EmbedBuilder()
-                    .setColor(0x0099FF)
-                    .setTitle(`${data.login} Profile`)
-                    .setURL(`https://github.com/${desiredProfile}`)
-                    .setThumbnail("https://i.pinimg.com/originals/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.png")
-                    .addFields(
-                        { name: "Nickname", value: `${data.login}` },
-                        { name: "Name", value: `${data.name}` },
-                        { name: "Repositories (Public)", value: `${data.public_repos}` },
-                        { name: "Followers", value: `${data.followers}` },
-                        { name: "Following", value: `${data.following}` },
-                        { name: "Website", value: `${data.blog ? data.blog : "None"}` }
-                    )
-                    .setImage(`${data.avatar_url}`);
-                await message.reply({ embeds: [embed] });
-            } catch (error) {
-                message.reply(`An error ocurred. ${error}`);
-            }
+    if (!message.author.bot && messageText.startsWith("!")) {
+        const desiredComand = messageText.substring(0, messageText.indexOf(" "));
+        switch (desiredComand) {
+            case "!profile":
+                const desiredProfile = messageText.substring(messageText.indexOf(" ") + 1, messageText.length);
+                try {
+                    const url = `https://api.github.com/users/${desiredProfile}`
+                    const response = await fetch(url);
+                    const data = await response.json();
+                    console.log(data);
+                    const embed = new EmbedBuilder()
+                        .setColor(0x0099FF)
+                        .setTitle(`${data.login} Profile`)
+                        .setURL(`https://github.com/${desiredProfile}`)
+                        .setThumbnail("https://i.pinimg.com/originals/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.png")
+                        .addFields(
+                            { name: "Nickname", value: `${data.login}` },
+                            { name: "Name", value: `${data.name}` },
+                            { name: "Repositories (Public)", value: `${data.public_repos}` },
+                            { name: "Followers", value: `${data.followers}` },
+                            { name: "Following", value: `${data.following}` },
+                            { name: "Website", value: `${data.blog ? data.blog : "None"}` }
+                        )
+                        .setImage(`${data.avatar_url}`);
+                    await message.reply({ embeds: [embed] });
+                } catch (error) {
+                    console.log(error);
+                    message.reply("An error ocurred. We could not find the desired profile. Try again!");
+                }
+                break;
+            case "!repo":
+
+                break;
+            default:
+                message.reply("Command not found. Try '!profile {name}' or '!repo {name}'")
+                break;
         }
     }
 });
